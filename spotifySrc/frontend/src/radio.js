a3882e2356c63a68d5d2a928d217b7988a77398c
alanis <alanis_narvaez@icloud.com>
2023-10-18 20:25:21 -0400

    getting correct response from backend, need changing pages

---
+++ spotifySrc/frontend/src/radio.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import Spotify from 'react-spotify-embed/dist/package.json'
+// import { Container } from 'react-bootstrap'
+
+const Radio = () => {
+    return (
+        <div>
+            <p>Here goes the embedded player and audio visualizer background</p>
+        </div>
+    );
+}
+
+export default Radio;
+
+/* <div>
+<Container>
+  <h1>Iframe Demo</h1>
+  <iframe title="Spotify Web Player" src={`https://open.spotify.com/embed${url.pathname}`} width={300} height={380} allow="encrypted-media" style={{borderRadius: 8}}/>
+</Container>
+</div> */
+
+// const spotify = "<Spotify link={response.json()['url']} />"
+// const iframe = '<iframe title="Spotify Web Player" src={`https://open.spotify.com/embed${url.pathname}`} width={300} height={380} allow="encrypted-media" style={{borderRadius: 8}}/>'
+
+// function Iframe(props) {
+//   return (<div dangerouslySetInnerHTML={ {__html:  props.iframe?props.iframe:""}} />);
+// }
