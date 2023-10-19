eb20727db4e6bf11edfb4dde31082877262cd2a6
alanis <alanis_narvaez@icloud.com>
2023-10-18 22:49:12 -0400

    spotify player renders now

--- spotifySrc/frontend/src/index.js
+++ spotifySrc/frontend/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
-import Radio from './radio';
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
@@ -11,10 +10,9 @@ document.body.innerHTML = '<div id="app"></div>';
 const root = createRoot(document.getElementById('app'));
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/radio' element={<Radio />} />
-    </Routes>
-  </BrowserRouter>
+    <Router>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Router>
 );
