4c209515ef011123f6f0db807ccfa84b553d2b0c
alanis <alanis_narvaez@icloud.com>
2023-12-04 04:11:04 -0500

    done except for multiple next issue and sort

---
+++ src/backend/endpoints2.py
@@ -0,0 +1,128 @@
+'''
+Name: idk (Borealis?)
+
+Description:
+- No authentication required for v0.5
+    - Could add auth for v1 to add to library
+- simple UI, only audio visualization, current song, upcoming song
+- opening page is search bar with app name above it
+- Background is sound waves of current song play
+    - before song input it will be default waves
+    - if song is paused waves stop
+- after first song input, embed spotify player over waves
+    - spotify player oEmbed API
+    - waves will implement first data structure
+- allow add to library
+'''
+
+import os
+
+from spot import Spot
+from radio import Radio
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pyautogui
+
+HOST_IP_ADDRESS = 'localhost'
+HOST_PORT = '8080'
+
+# Env vars
+# USERNAME = os.environ['SpotifyUser']
+CLIENT_ID = os.environ.get('BorealisCID')
+CLIENT_SECRET = os.environ.get('BorealisSecret')
+
+SCOPE = 'user-library-read user-library-modify'
+REDIRECT_URI = 'http://localhost:8080/start'
+
+app = Flask('Borealis')
+CORS(app)
+
+'''
+Test env vars can be read (for mac in ~/.zshenv)
+print(CLIENT_ID)
+print(CLIENT_SECRET)
+'''
+
+spot = Spot(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+radio = Radio(spot) 
+
+@app.route('/start', methods=['POST', 'GET'])
+def start():
+    if spot.callback is False:
+        print(request.get_json()) # remove this
+        # grab user front-end 
+        response = request.get_json()
+        radio.start_song = response['song']
+        radio.start_artist = response['artist']
+        
+        # authenticate with api
+        spot.authorize(SCOPE)
+        while radio.curr_id == '':
+            continue
+    else:
+        callback()
+        spot.callback = False
+
+
+    # return oEmbed API response for spotify player and visuals
+    ret = radio.songEmbed(radio.curr_id)['url']
+    radio.curr_id = ''
+    return {'url':ret}
+
+def callback():
+    code = request.args.get('code')
+    credentials = spot.get_token(code)
+    spot.ACCESS_TOKEN = credentials['access_token']
+
+    spot.get_profile()
+
+    radio.curr_id = radio.generate()
+    # visualize = spot.audio_analysis(song_id) # implement
+    print(radio.curr_id)
+
+# @app.route('/analyze', methods=['POST'])
+# def analyze():
+#     print(request.get_json())
+#     response = request.get_json()
+#     song_id = response['id']
+
+#     segments = spot.request('https://api.spotify.com/v1/audio-analysis/'+song_id, {})['segments']
+#     return {'segments':segments}
+
+@app.route('/upNext', methods=['GET'])
+def upNext():
+    return jsonify(next=radio.queue[-1])
+
+# return player html for next song
+@app.route('/next', methods=['GET'])
+def next():
+    print(len(radio.queue)) # FOR TESTING
+    track = radio.queue.pop()
+    radio.seed_tracks.add(track['id'])
+    radio.now_playing = track['id']
+
+    return {'url':track['url']}
+
+# add track to liked songs
+@app.route('/like', methods=['PUT'])
+def like():
+    # print(f'now playing {radio.now_playing}')
+    res = spot.addToLib(radio.now_playing)
+
+    return {'status_code': res}
+
+@app.route('/action', methods=['POST'])
+def action():
+    act = request.args['action']
+    song_id = request.args['song_id']
+    if act == 'dislike':
+        # re-generate queue
+        radio.seed_tracks.discard(song_id)
+        radio.resetQueue()
+        return next()
+    else:
+        # add to user spotify library
+        pass
+
+if __name__ == '__main__':
+    app.run(host=HOST_IP_ADDRESS, port=HOST_PORT, use_reloader=True, debug=True)
---
+++ src/frontend/src/App.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+
+function App() {
+    const navigate = useNavigate();
+
+    const [song, setSong] = useState('');
+    const [artist, setArtist] = useState('');
+    const [characteristic, setCharac] = useState('');
+    const [sort, setSort] = useState('');
+
+    useEffect(() => {
+        // Reset the input values when the component mounts (user navigates back)
+        return () => {
+            setSong('');
+            setArtist('');
+            setCharac('');
+            setSort('');
+        };
+    }, []);
+
+    const Grab = async (e) => {
+        e.preventDefault()
+        if (!(song.trim() === '' || artist.trim() === '')) {
+            const data = {
+                song: song.trim(),
+                artist: artist.trim(),
+                characteristic: characteristic,
+                sort: sort
+
+            };
+    
+            const response = await fetch('/start', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+    
+            if (response.ok) {
+                const responseData = await response.json();
+                console.log(responseData)
+    
+                navigate('/radio', { state: { url: responseData.url } });
+            } else { // add non-song alert
+                alert("Error occurred while fetching data");
+            }
+        }
+    };
+
+    return (
+        <div className="App aurora-outer">
+            <section id="up" />
+            <section id="down" />
+            <section id="left" />
+            <section id="right" />
+            <div className="box stuff">
+                <h1>Borealis</h1>
+                <form onSubmit={Grab}>
+                    <input
+                        type="text"
+                        value={song}
+                        placeholder='Song'
+                        onChange={(e) => setSong(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        value={artist}
+                        placeholder='Artist'
+                        onChange={(e) => setArtist(e.target.value)}
+                    />
+
+                    <h2>Sort by...</h2>
+                    <div className='dropdown'>
+                        <select name="characteristic" id="characteristic" onChange={(e) => setCharac(e.target.value)}>
+                            <option value="" disabled selected id="placeholder">Characteristic</option>
+                            <option value="none">None</option>
+                            <option value="acousticness">Acousticness</option>
+                            <option value="danceability">Danceability</option>
+                            <option value="energy">Energy</option>
+                            <option value="instrumentalness">Instrumentalness</option>
+                            <option value="loudness">Loudness</option>
+                            <option value="valence">Valence</option>
+                            <option value="key">Key</option>
+                            <option value="tempo">Tempo</option>
+                        </select>
+
+                        <select name="sorting" id="sorting" onChange={(e) => setSort(e.target.value)}>
+                            <option value="" disabled selected id="placeholder">Algorithm</option>
+                            <option value="none">None</option>
+                            <option value="merge">Mergesort</option>
+                            <option value="smooth">Smoothsort</option>
+                        </select>
+                    </div>
+
+                    <button type="submit">Play</button>
+                </form>
+            </div>
+        </div> 
+    );
+}
+
+export default App;
